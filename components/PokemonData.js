dd795a5dabbef4db77e7b15ca3d7130587edc978
Joey Walton <Joeydw10@hotmail.com>
2023-03-15 16:56:00 -0400

    working on useEffect for pokedex

--- components/PokemonData.js
+++ components/PokemonData.js
@@ -1,3 +1,42 @@
-export function PokemonData(){
-    
+import Link from "next/link";
+import { Button, Card } from "react-bootstrap";
+import useSWR from "swr";
+
+
+export function PokemonData(prop) {
+  const { data, error } = useSWR(
+    `https://pokeapi.co/api/v2/pokemon/${prop.id}`
+  );
+  if (error) {
+    return <Error statusCode={404} />;
+  }
+  if (!data) {
+    return null;
+  }
+  return (
+    <Card>
+      <Card.Img
+        src={
+          data?.sprites.other["official-artwork"].front_default ||
+          "https://via.placeholder.com/50x50.png?text=%5B+Not+Available+%5D"
+        }
+      />
+      <Card.Body>
+        <Card.Title>{data?.name || "N/A"}</Card.Title>
+        <Card.Text>
+          <strong>ID: </strong> {data?.id || "N/A"}
+          <br />
+          <strong>Classification:</strong> {data?.classification || "N/A"}
+          <br />
+          <strong>Medium:</strong> {data?.medium || "N/A"} <br />
+        </Card.Text>
+        <Link href={`/artwork/${data?.objectID}`} passHref>
+          <Button variant="dark">
+            <strong>ID: </strong>
+            {data?.objectID}
+          </Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,82 @@
+import { PokemonData } from "@/components/PokemonData"
+import Error from "next/error";
+import { useEffect, useState } from "react"
+import { Card, Col, Pagination, Row } from "react-bootstrap";
+import useSWR from "swr";
+
+
+const PER_PAGE = 12;
 
 export default function Home() {
+  const [pokemonList, setPokemonList] = useState();
+  const [page, setPage] = useState(1);
+  const {data, error} = useSWR(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+
+  const previousPage = () =>{
+    if(page > 1){
+      setPage(page - 1);
+    }
+  }
+  const nextPage = () =>{
+    if (page < pokemonList.length){
+      setPage(page + 1)
+    }
+  }
+
+  useEffect(() =>{
+    let results = [];
+    if(data){
+      for (let i = 0; i < data?.length; i += PER_PAGE) {
+        const chunk = data?.id.slice(i, i + PER_PAGE);
+        results.push(chunk);
+      }
+      setPokemonList(results);
+      setPage(1);
+    }
+  }, [data]);
+
+  if (error){
+    return <Error statusCode={404}/>;
+  }
+  if (!pokemonList){
+    return null;
+  }
+
   return (
     <>
-    <p>
-      hello
-    </p>
+    {/* <PokemonData id={151}/> */}
+    {pokemonList.length > 0 ? (
+        <Row className="gy-4">
+          {pokemonList[page - 1].map((id) => {
+            return (
+              <Col lg={3} key={id}>
+                <PokemonData id={id} />
+              </Col>
+            );
+          })}
+        </Row>
+      ) : (
+        <Row>
+          <Col>
+            <Card>
+              <h4>Nothing Here</h4>
+              Try searching for something else.
+            </Card>
+          </Col>
+        </Row>
+      )}
+
+      {pokemonList.length > 0 ? (
+        <Row>
+          <Col>
+            <Pagination>
+              <Pagination.Prev onClick={previousPage} />
+              <Pagination.Item active>{page}</Pagination.Item>
+              <Pagination.Next onClick={nextPage} />
+            </Pagination>
+          </Col>
+        </Row>
+      ) : null}
     </>
   )
 }
